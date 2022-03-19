31f611c2cfe33c34a3ff92e14633add101c9b820
Dev-JeanSantos <jeancbsan@gmail.com>
2022-03-19 19:22:49 -0300

    criando projeto

---
+++ src/main/java/com/teste/treinamento_spring/TreinamentoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.teste.treinamento_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreinamentoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreinamentoSpringApplication.class, args);
+	}
+
+}
