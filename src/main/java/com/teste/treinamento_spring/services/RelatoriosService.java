bd44671d8132f9728ec0b79e0ac785933b30036f
Dev-JeanSantos <jeancbsan@gmail.com>
2022-03-27 18:36:36 -0300

    Implementado relatório para buscar funcionarios por nome

--- src/main/java/com/teste/treinamento_spring/services/RelatoriosService.java
+++ src/main/java/com/teste/treinamento_spring/services/RelatoriosService.java
@@ -2,18 +2,26 @@ package com.teste.treinamento_spring.services;
 
 import com.teste.treinamento_spring.orms.Funcionario;
 import com.teste.treinamento_spring.repositories.FuncionarioRepository;
+import com.teste.treinamento_spring.responses.FuncionarioResponse;
+import com.teste.treinamento_spring.services.exceptions.ResourcesNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class RelatoriosService {
-    private Boolean system = true;
+
+    @Autowired
     private FuncionarioRepository repository;
 
-    public void relatorio(String nome) {
-        List<Funcionario> funcionarios = repository.findByNome(nome);
-        funcionarios.forEach(System.out::println);
+    @Transactional
+    public FuncionarioResponse relatorioPorNome(String nome) {
+        Optional<Funcionario> obj = repository.findByNome(nome);
+        Funcionario funcionario = obj.orElseThrow(() -> new ResourcesNotFoundException ("Funcionario não enontrado"));
+        return new FuncionarioResponse(funcionario, funcionario.getUnidadeTrabalhos());
 
     }
 }
